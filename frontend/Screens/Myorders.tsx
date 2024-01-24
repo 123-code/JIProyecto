e50e9a6112328278c700af1bf5b2d3e5d34c4801
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2024-01-23 20:23:28 -0500

    Update Myorders.tsx

--- frontend/Screens/Myorders.tsx
+++ frontend/Screens/Myorders.tsx
@@ -1,114 +1,98 @@
-import React, { useState,useEffect } from 'react';
-import { View, Text, TextInput, Button, StyleSheet,Pressable } from 'react-native';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet,TouchableOpacity } from 'react-native';
 import { Card, Title, Paragraph } from 'react-native-paper';
-
-//import OrderCard  from '../Components/OrderCard'
+import axios from 'axios';
 
 interface Order {
-    nombre: string;
-    cantidad: number; 
-    Email: string;
-    CreatedAt: string;
-  }
-  
+  nombre: string;
+  cantidad: number;
+  Email: string;
+  CreatedAt: string;
+}
 
-export default function Myorders({navigation,route}:any){
+export default function Myorders({ navigation, route }: any) {
   const [mycryptoData, setmyCryptoData] = useState<Order[]>([]);
- 
 
-  useEffect (()=>{
-    const ViewOrders = async () => {
-        try {
-            const response = await axios.get('http://localhost:8080/getalldata')
-            setmyCryptoData(response.data);
-    
-          
-         // console.log(myorders.data);
-        } catch (error) {
-            console.error('Error fetching data: ', error);
-            window.alert('Error fetching data');
-          }
-      }; 
-      ViewOrders(); 
-  },[])
- 
-  
+  useEffect(() => {
+    const viewOrders = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/getalldata');
+        setmyCryptoData(response.data);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+        window.alert('Error fetching data');
+      }
+    };
+    viewOrders();
+  }, []);
+
   return (
     <View style={styles.container}>
-         <View style={styles.header}>
-         <Text style={styles.txt}> Mis Órdenes</Text>
-         </View>
-     {mycryptoData.map((order,index)=>(
-        <Card key={index} style={styles.card} onPress={()=>{navigation.navigate('Datascreen' ,{order: order})}}
-        >
-      <Card.Content>
-            <Title style={ styles.titlecardText}>{order.nombre}</Title>
-            <Paragraph style={styles.cardText}>${order.cantidad}</Paragraph>
-            <Paragraph style={styles.cardText}>{order.Email}</Paragraph>
-            <Paragraph style={styles.cardText}>{new Date(order.CreatedAt).toLocaleDateString('en-US', {
-                                                month: '2-digit',
-                                                day: '2-digit',
-                                                year: '2-digit'
-                                                 })}</Paragraph>
-                                   </Card.Content>
-                                    </Card>
-                                    ))}
-                                     </View>
-    
+      <View style={styles.header}>
+        <Text style={styles.txt}>Órdenes disponibles</Text>
+      </View>
+      <View style={styles.cardContainer}>
+        {mycryptoData.map((order, index) => (
+          <Card
+            key={index}
+            style={styles.card}
+            onPress={() => {
+              navigation.navigate('Datascreen', { order: order });
+            }}
+          >
+            <Card.Content>
+              <Title style={styles.titlecardText}>{order.nombre}</Title>
+              <Paragraph style={styles.cardText}>${order.cantidad}</Paragraph>
+              <Paragraph style={styles.cardText}>{order.Email}</Paragraph>
+              <Paragraph style={styles.cardText}>
+                {new Date(order.CreatedAt).toLocaleDateString('en-US', {
+                  month: '2-digit',
+                  day: '2-digit',
+                  year: '2-digit',
+                })}
+              </Paragraph>
+            </Card.Content>
+          </Card>
+        ))}
+      </View>
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => {
+          navigation.navigate("NewOrder")
+        }}
+      >
+        <Text style={styles.addButtonText}>+</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
- container: {
+  container: {
     flex: 1,
     backgroundColor: '#212121',
     padding: 10,
   },
-  card: {
+  cardContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  card: {
+    width: '48%', // Adjust the width as needed
+    marginBottom: 10,
     backgroundColor: '#39FF14',
     borderRadius: 28,
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-    marginLeft: 'auto',
-    marginRight: 'auto',
     elevation: 3,
-    marginBottom: 10,
-    marginTop: 20,
-    width: '70%',
   },
-  
   cardText: {
     fontSize: 16,
-  
     color: 'black',
     fontFamily: 'Futura',
     letterSpacing: 1.2,
-    flex: 1,
     textAlign: 'center',
   },
-  input: {
-    alignItems: 'center',
-    height: 40,
-    width: '100%',
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 38,
-    marginBottom: 15,
-    paddingLeft: 10,
-    backgroundColor: 'white',
-  },
-  main: {
-    flex: 1,
-    backgroundColor: '#212121',
-    borderRadius: 10,
-    padding: 20,
-    alignItems: 'center',
-  },
-  txt:{
+  txt: {
     color: 'white',
     fontSize: 20,
     textAlign: 'center',
@@ -117,17 +101,30 @@ const styles = StyleSheet.create({
   },
   titlecardText: {
     fontSize: 16,
-  fontWeight:'bold',
+    fontWeight: 'bold',
     color: '#777',
     fontFamily: 'Futura',
     letterSpacing: 1.2,
-    flex: 1,
     textAlign: 'center',
   },
-  header: { 
-    
+  header: {
     paddingVertical: 16,
-    paddingHorizontal: 12
+    paddingHorizontal: 12,
   },
-
+  addButton: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+    backgroundColor: '#39FF14',
+    borderRadius: 50,
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 3,
+  },
+  addButtonText: {
+    fontSize: 24,
+    color: 'black',
+  },
 });
